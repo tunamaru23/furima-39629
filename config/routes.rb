bcf39c088015965d8e0a10481b911ed0f700ec3b
tunamaru23 <yusukem0023@gmail.com>
2023-11-02 10:19:42 +0900

    デプロイ時追加忘れ

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  #root to: 'devise/registrations#new'
+  root to: 'items#index'
+  resources :items, only:[:index, :new, :create,:show, :destroy]
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+
 end
