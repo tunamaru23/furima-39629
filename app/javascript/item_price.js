3a5c0daa1b4cbb69befa27cc660a79090e29eedf
tunamaru23 <yusukem0023@gmail.com>
2023-11-08 17:20:55 +0900

    item_price 関数の実装を改善

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,10 +1,17 @@
-window.addEventListener('load', function(){
+function item_price() {
   const priceInput = document.getElementById("item-price");
   priceInput.addEventListener("input", () => {
     const inputValue = priceInput.value;
+    const fee = Math.floor(inputValue * 0.1);
+    const profit = Math.floor(inputValue - fee);
+
     const addTaxDom = document.getElementById("add-tax-price");
-    addTaxDom.innerHTML = Math.floor(inputValue * 0.1);
+    addTaxDom.innerHTML = fee;
+
     const addProfitDom = document.getElementById("profit");
-    addProfitDom.innerHTML = Math.floor(inputValue - Math.floor(inputValue * 0.1))
-  })
-})
+    addProfitDom.innerHTML = profit;
+  });
+}
+
+window.addEventListener('load', item_price);
+window.addEventListener("turbo:render", item_price);
